94e5b840d61ae3ecc832c7d157e4e5846df7f064
hoangbkcntt98 <root@DESKTOP-A39S8L4>
2023-07-26 00:38:22 +0900

    refactoring

--- environment/composer/create_project.sh
+++ environment/composer/create_project.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 if [ ! -d "/var/www/html/$APP_NAME" ]; then
     composer create-project --prefer-dist laravel/laravel /var/www/html/$APP_NAME
-    cd /var/www/html/$APP_NAME
-    composer install 
-    chmod -R 777 ./storage
 fi
--- environment/php/init.sh
+++ environment/php/init.sh
@@ -1,3 +1,9 @@
+cd /var/www
+sed -i "s/^DB_HOST=.*$/DB_HOST=database/" .env
+sed -i "s/^DB_DATABASE=.*$/DB_DATABASE=$APP_NAME/" .env
+sed -i "s/^DB_USERNAME=.*$/DB_USERNAME=$DB_USER/" .env
+sed -i "s/^DB_PASSWORD=.*$/DB_PASSWORD=$DB_PASSWORD/" .env
 composer install 
 php artisan key:generate
 php artisan migrate
+chmod -R 777 ./storage
